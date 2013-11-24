e7a01c6ab63a27eeb82228730cae757bcbcaf8ce
fboris <fantasyboris@gmail.com>
2013-11-24 21:01:41 +0800

    LCD will show time when auto wake up event happened I'm not sure the binary data is correct or not. Need to test it.

--- RTC.c
+++ RTC.c
@@ -20,11 +20,18 @@ void RTC_Alarm_IRQHandler()
 }
 void RTC_WKUP_IRQHandler(void)
 {
+  RTC_TimeTypeDef RTC_TimeStruct;
   if(RTC_GetITStatus(RTC_IT_WUT) != RESET)
   {
     RTC_ClearITPendingBit(RTC_IT_WUT);
     EXTI_ClearITPendingBit(EXTI_Line22);
     STM_EVAL_LEDToggle(LED6);
+    
+    RTC_SetTime(RTC_Format_BIN, &RTC_TimeStruct);
+
+    showCalendar_time(RTC_TimeStruct.RTC_Hours,
+                      RTC_TimeStruct.RTC_Minutes,
+                      RTC_TimeStruct.RTC_Seconds);
   }
 }
 static void initialize_RTC(void)
